a6b06071092c71679d3cab7438b66fe24010e5b1
Guilherme Girotto <guiga741@gmail.com>
2020-04-18 09:59:52 -0300

    Fixes Proxy server crash due to socket hanged up

--- main/src/electron.ts
+++ main/src/electron.ts
@@ -27,18 +27,27 @@ function startWindow() {
 }
 
 function startProxy() {
-    const proxy = hoxy.createServer({
+    const proxyServer = hoxy.createServer({
         certAuthority: {
             key: fs.readFileSync(`${__dirname}/my-private-root-ca.key.pem`),
             cert: fs.readFileSync(`${__dirname}/my-private-root-ca.crt.pem`)
           }
-    }).listen(8080)
+    })
+
+    proxyServer.on('error', (event: any) => {
+        // Fallback to "socket hang up" error
+        // ENOTFOUND means the target URL was not found -> it may not exists and DNS couldn't resolve it
+        if (event.code === 'ENOTFOUND') return
+        console.warn('hoxy error: ', event.code, event)
+      });
+  
+    proxyServer.listen(8080)
 
     mainWindow.webContents.on('did-finish-load', () => {
         // 'did-finish-load' may be called multiple times when debugging with React Hot Reload
         if (!didSetupProxy) {
             didSetupProxy = true
-            proxy.intercept('request', (req, res, cycle) => {
+            proxyServer.intercept('request', (req, res, cycle) => {
                 mainWindow.webContents.send('proxy-new-request', { headers: req.headers, protocol: req.protocol, hostname: req.hostname, method: req.method, url: req.url });
             })
         }
