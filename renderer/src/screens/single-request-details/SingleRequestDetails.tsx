561d68d3a934a0f3614c0c14519ec683671cd234
Guilherme Girotto <guiga741@gmail.com>
2020-05-03 14:45:19 -0300

    Adds Request Information Table

--- renderer/src/screens/single-request-details/SingleRequestDetails.tsx
+++ renderer/src/screens/single-request-details/SingleRequestDetails.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
-// Models
 import { RequestCycle } from '../../models'
 
-// Components
 import { SingleRequestCardsCollection } from '../../components/cards-collection/RequestsCardsCollection'
 import SegmentedControl from '../../components/segmented-control/SegmentedControl'
+import RequestInfoTable from './components/RequestInfoTable'
 
-// Style
 import './SingleRequestDetails.css'
+import '../../extensions/string'
 
 class SingleRequestState {
     constructor(public selectedSegmentIndex: number = 0) {}
@@ -17,11 +16,13 @@ class SingleRequestState {
 const SingleRequestDetails = (props: { selectedCycle: RequestCycle }) => {
     const [state, setState] = useState(new SingleRequestState())
 
-    const formattedTitle = props.selectedCycle.fullUrl.length > 32 ? `${props.selectedCycle.fullUrl.substring(0, 32)}...` : props.selectedCycle.fullUrl
+    const formattedTitle = props.selectedCycle.fullUrl.clippedTo(32)
     const segmentItems = ["General Info", "Request", "Response"]
 
     const segmentSelectionHandler = (selectedIndex: number) => {
-        console.log("Did select index " + selectedIndex)
+        setState({
+            ...state, selectedSegmentIndex: selectedIndex
+        })
     }
 
     return (
@@ -30,6 +31,7 @@ const SingleRequestDetails = (props: { selectedCycle: RequestCycle }) => {
                 <h1>{formattedTitle}</h1>
                 <SingleRequestCardsCollection cycle={props.selectedCycle} />
                 <SegmentedControl items={segmentItems} selectionHandler={segmentSelectionHandler} />
+                <RequestInfoTable request={props.selectedCycle} />
             </div>
         </>
     )
